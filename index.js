3db28021016289377eae0235996569e0d0508aa7
barrettanderson <barrettanderson1989@gmail.com>
2020-09-09 14:13:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]+/g, '' )
+  }
+
+  static titleize(sentence) {
+    let ignoredWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let array = sentence.split(" ")
+    let capitalized = array.map(word => {
+      if(!ignoredWords.includes(word)) {
+        return this.capitalize(word)
+      } else { return word }
+    })
+    capitalized[0] = this.capitalize(capitalized[0])
+    return capitalized.join(" ")
+  }
 }
